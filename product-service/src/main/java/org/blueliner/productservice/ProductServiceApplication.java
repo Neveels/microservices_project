0a62e82a761877cabe14aabee8d7dda28c3eabbd
nevels <neevels@mail.ru>
2023-08-20 17:06:05 +0300

    init for product service

---
+++ product-service/src/main/java/org/blueliner/productservice/ProductServiceApplication.java
@@ -0,0 +1,13 @@
+package org.blueliner.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceApplication.class, args);
+    }
+
+}
